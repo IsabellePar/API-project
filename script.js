0cc415c01bf7a8fb317113eaa348fcbe426169d9
unknown <72137459+andrkavr@users.noreply.github.com>
2024-07-31 16:47:49 +0200

    added search function and simplistic display of country name and flag for search results

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
-console.log("Hello, World!");
-
 async function countryFetch(countryName) {
   let response = await fetch(
     `https://restcountries.com/v3.1/name/${countryName}`
   );
   let data = await response.json();
-  let relevant = {
-    country: data[0].name.common,
-  };
+  let relevant = await data.filter((element) =>
+    element.name.common.toLowerCase().includes(countryName.toLowerCase())
+  );
+  //   console.log(relevant);
 
-  return data;
+  return relevant;
 }
 
-async function logCountryData(str) {
-  try {
-    let data = await countryFetch(str);
+//event listener
+let formRef = document.querySelector("#searchForm");
+formRef.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  let text = event.target.searchInput.value;
+  console.log(text);
+  let test = await countryFetch(text);
+  console.log(test);
+  cardMaker(test);
+});
 
-    data.forEach((element) => {
-      if (element.name.common.toLowerCase().includes(str.toLowerCase())) {
-        console.log(element.name.common);
-      }
-    });
-  } catch (error) {
-    console.error(error);
-  }
+function cardMaker(data) {
+  let searchResults = document.querySelector("#searchResults");
+  searchResults.innerHTML = "";
+  data.forEach((element) => {
+    let card = document.createElement("article");
+    card.classList.add("card");
+
+    card.innerHTML = `
+  <figure>
+    <img src="${element.flags ? element.flags.png : ""}" alt="Flag of ${
+      element.name.common
+    }">
+  </figure>
+  <div>
+    <h3>${element.name.common}</h3>
+  </div>
+`;
+    searchResults.appendChild(card);
+  });
 }
-
-logCountryData("den");
